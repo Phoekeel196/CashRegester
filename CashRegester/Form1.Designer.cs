5180bd1b8606728a8fa3ed8f47bb90748b9a50b9
Phoenix Keeling <phoekeel196@outlook.com>
2020-11-26 14:29:22 -0500

    Add project files.

---
+++ CashRegester/Form1.Designer.cs
@@ -0,0 +1,368 @@
+﻿namespace CashRegester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TBMystery = new System.Windows.Forms.TextBox();
+            this.TBBottle = new System.Windows.Forms.TextBox();
+            this.TBPizza = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.AlmostTotal = new System.Windows.Forms.Label();
+            this.GShares = new System.Windows.Forms.Label();
+            this.RipOff = new System.Windows.Forms.Label();
+            this.ChangeBox = new System.Windows.Forms.Label();
+            this.Order = new System.Windows.Forms.Label();
+            this.Stuff = new System.Windows.Forms.Label();
+            this.BillBox = new System.Windows.Forms.TextBox();
+            this.Clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(598, 473);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Maroon;
+            this.label2.Font = new System.Drawing.Font("Mistral", 53F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(588, 84);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Filthy Franks Greaseria";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Maroon;
+            this.label3.Font = new System.Drawing.Font("Mistral", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label3.Location = new System.Drawing.Point(22, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "DeepFried DeepDish Pizza";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Maroon;
+            this.label4.Font = new System.Drawing.Font("Mistral", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label4.Location = new System.Drawing.Point(21, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(228, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Filthy Franks Specialty Dumpster Mystery\r\n";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Maroon;
+            this.label5.Font = new System.Drawing.Font("Mistral", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label5.Location = new System.Drawing.Point(22, 127);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 19);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Grease In A Bottle";
+            // 
+            // TBMystery
+            // 
+            this.TBMystery.BackColor = System.Drawing.Color.Maroon;
+            this.TBMystery.Font = new System.Drawing.Font("MisterEarl BT", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBMystery.Location = new System.Drawing.Point(257, 154);
+            this.TBMystery.Name = "TBMystery";
+            this.TBMystery.Size = new System.Drawing.Size(57, 28);
+            this.TBMystery.TabIndex = 5;
+            // 
+            // TBBottle
+            // 
+            this.TBBottle.BackColor = System.Drawing.Color.Maroon;
+            this.TBBottle.Font = new System.Drawing.Font("MisterEarl BT", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBBottle.Location = new System.Drawing.Point(257, 122);
+            this.TBBottle.Name = "TBBottle";
+            this.TBBottle.Size = new System.Drawing.Size(57, 28);
+            this.TBBottle.TabIndex = 6;
+            // 
+            // TBPizza
+            // 
+            this.TBPizza.BackColor = System.Drawing.Color.Maroon;
+            this.TBPizza.Font = new System.Drawing.Font("MisterEarl BT", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBPizza.Location = new System.Drawing.Point(257, 90);
+            this.TBPizza.Name = "TBPizza";
+            this.TBPizza.Size = new System.Drawing.Size(57, 28);
+            this.TBPizza.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Maroon;
+            this.button1.Font = new System.Drawing.Font("MisterEarl BT", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(26, 186);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(288, 43);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Calculate Over Priced Food";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Maroon;
+            this.label6.Font = new System.Drawing.Font("MisterEarl BT", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(22, 240);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 24);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Almost Total";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Maroon;
+            this.label7.Font = new System.Drawing.Font("MisterEarl BT", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(22, 275);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(110, 24);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Goverments Share";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Maroon;
+            this.label8.Font = new System.Drawing.Font("MisterEarl BT", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(21, 312);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 24);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Total";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Maroon;
+            this.label9.Font = new System.Drawing.Font("MisterEarl BT", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label9.Location = new System.Drawing.Point(20, 346);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(72, 29);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Payment";
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.BackColor = System.Drawing.Color.Maroon;
+            this.button2.Font = new System.Drawing.Font("MisterEarl BT", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(25, 378);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(289, 34);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Change";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Maroon;
+            this.label10.Font = new System.Drawing.Font("MisterEarl BT", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(22, 415);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(52, 24);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "Change";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Maroon;
+            this.button3.Font = new System.Drawing.Font("MisterEarl BT", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(24, 442);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(290, 35);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Proof";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // AlmostTotal
+            // 
+            this.AlmostTotal.BackColor = System.Drawing.Color.Maroon;
+            this.AlmostTotal.Font = new System.Drawing.Font("MisterEarl BT", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AlmostTotal.Location = new System.Drawing.Point(136, 240);
+            this.AlmostTotal.Name = "AlmostTotal";
+            this.AlmostTotal.Size = new System.Drawing.Size(178, 24);
+            this.AlmostTotal.TabIndex = 16;
+            // 
+            // GShares
+            // 
+            this.GShares.BackColor = System.Drawing.Color.Maroon;
+            this.GShares.Font = new System.Drawing.Font("MisterEarl BT", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GShares.Location = new System.Drawing.Point(136, 275);
+            this.GShares.Name = "GShares";
+            this.GShares.Size = new System.Drawing.Size(178, 23);
+            this.GShares.TabIndex = 17;
+            // 
+            // RipOff
+            // 
+            this.RipOff.BackColor = System.Drawing.Color.Maroon;
+            this.RipOff.Font = new System.Drawing.Font("MisterEarl BT", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RipOff.Location = new System.Drawing.Point(136, 312);
+            this.RipOff.Name = "RipOff";
+            this.RipOff.Size = new System.Drawing.Size(178, 23);
+            this.RipOff.TabIndex = 18;
+            // 
+            // ChangeBox
+            // 
+            this.ChangeBox.BackColor = System.Drawing.Color.Maroon;
+            this.ChangeBox.Font = new System.Drawing.Font("MisterEarl BT", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ChangeBox.Location = new System.Drawing.Point(133, 415);
+            this.ChangeBox.Name = "ChangeBox";
+            this.ChangeBox.Size = new System.Drawing.Size(181, 23);
+            this.ChangeBox.TabIndex = 20;
+            // 
+            // Order
+            // 
+            this.Order.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Order.Location = new System.Drawing.Point(320, 90);
+            this.Order.Name = "Order";
+            this.Order.Size = new System.Drawing.Size(280, 349);
+            this.Order.TabIndex = 21;
+            // 
+            // Stuff
+            // 
+            this.Stuff.AutoSize = true;
+            this.Stuff.BackColor = System.Drawing.Color.Maroon;
+            this.Stuff.Location = new System.Drawing.Point(136, 122);
+            this.Stuff.Name = "Stuff";
+            this.Stuff.Size = new System.Drawing.Size(0, 13);
+            this.Stuff.TabIndex = 22;
+            // 
+            // BillBox
+            // 
+            this.BillBox.BackColor = System.Drawing.Color.Maroon;
+            this.BillBox.Font = new System.Drawing.Font("MisterEarl BT", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BillBox.Location = new System.Drawing.Point(137, 346);
+            this.BillBox.Name = "BillBox";
+            this.BillBox.Size = new System.Drawing.Size(177, 31);
+            this.BillBox.TabIndex = 23;
+            // 
+            // Clear
+            // 
+            this.Clear.BackColor = System.Drawing.Color.Maroon;
+            this.Clear.Font = new System.Drawing.Font("MisterEarl BT", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Clear.Location = new System.Drawing.Point(323, 443);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(277, 34);
+            this.Clear.TabIndex = 24;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 483);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.BillBox);
+            this.Controls.Add(this.Stuff);
+            this.Controls.Add(this.Order);
+            this.Controls.Add(this.ChangeBox);
+            this.Controls.Add(this.RipOff);
+            this.Controls.Add(this.GShares);
+            this.Controls.Add(this.AlmostTotal);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TBPizza);
+            this.Controls.Add(this.TBBottle);
+            this.Controls.Add(this.TBMystery);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox TBMystery;
+        private System.Windows.Forms.TextBox TBBottle;
+        private System.Windows.Forms.TextBox TBPizza;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label AlmostTotal;
+        private System.Windows.Forms.Label GShares;
+        private System.Windows.Forms.Label RipOff;
+        private System.Windows.Forms.Label ChangeBox;
+        private System.Windows.Forms.Label Order;
+        private System.Windows.Forms.Label Stuff;
+        private System.Windows.Forms.TextBox BillBox;
+        private System.Windows.Forms.Button Clear;
+    }
+}
+
---
+++ CashRegester/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//Phoenix Keeling, November 26 2020
+
+namespace CashRegester
+{
+    public partial class Form1 : Form
+    {
+        Double DFDDP = 12.49;
+        Double GIAB = 5.99;
+        Double FFSDM = 21.99;
+        Double noDFDDP;
+        Double noGIAB;
+        Double noFFSDM;
+        Double DFDDPPrice;
+        Double GIAPPrice;
+        Double FFSDMPrice;
+        Double SubTotal;
+        Double Taxes = 0.13;
+        Double Tax;
+        Double Total;
+        Double Bill;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public double ChangeAmmount1 { get; set; }
+        public double ChangeAmmount2 { get => ChangeAmmount1; set => ChangeAmmount1 = value; }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                noDFDDP = Convert.ToDouble(TBPizza.Text);
+                noGIAB = Convert.ToDouble(TBBottle.Text);
+                noFFSDM = Convert.ToDouble(TBMystery.Text);
+
+                DFDDPPrice = noDFDDP * DFDDP;
+                GIAPPrice = noGIAB * GIAB;
+                FFSDMPrice = noFFSDM * FFSDM;
+
+                SubTotal = DFDDPPrice + GIAPPrice + FFSDMPrice;
+                AlmostTotal.Text = SubTotal.ToString("C");
+
+                Tax = Taxes * SubTotal;
+                GShares.Text = Tax.ToString("C");
+
+                Total = Tax + SubTotal;
+                RipOff.Text = Total.ToString("C");
+            }
+
+            catch
+            {
+                Order.Text = "Letters Are Not A Way To Count Food Orders";
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Bill = Convert.ToDouble(BillBox.Text);
+                ChangeAmmount1 = Bill - Total;
+                ChangeBox.Text = ChangeAmmount1.ToString("C");
+            }
+            catch
+            {
+                Order.Text = "Letters Are Not A PayMent Method";
+            }
+        }
+
+    }
+
+
+}
+
+
+
+
+
+
+
